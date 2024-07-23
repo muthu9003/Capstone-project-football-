4b3a5910aab3bfb1418d70cb860cad2d6da062ea
muthu9003 <154418946+muthu9003@users.noreply.github.com>
2024-07-23 14:07:30 +0530

    Football sql.sql
    I had done some team and player analysis based on given football data

--- sql.sql
+++ sql.sql
@@ -1,21 +1,28 @@
 use football;
 select*from foot_ball;
 
+#Task 1 Identify the average market value of players with respect to playing position?
+
 SELECT AVG(market_value_in_eur) AS average_market_value
 FROM foot_ball
 WHERE position_y = 'attack';
 
+#Task 2 Find the count of players, year-wise in which players contract are about to expire? 
+  
 SELECT YEAR(contract_expiration_date) AS expiry_year, COUNT(*) AS player_count
 FROM foot_ball
 GROUP BY expiry_year
 ORDER BY player_count;
 
+#Task 3 Which referee had issued more number of red cards?
 
 SELECT referee, SUM(red_cards) AS total_red_cards
 FROM foot_ball
 GROUP BY referee
 ORDER BY total_red_cards DESC
 LIMIT 1;
+
+#Task 4 Find out the type of football competition,which has engaged more minutes?
 
 SELECT competition_type, SUM(minutes_played) AS total_minutes_played
 FROM foot_ball
